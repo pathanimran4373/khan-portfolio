f750290d31b91d3da86694583e4be5f306d9894c
Your Name <pathanimran4373@gmail.com>
2024-02-08 01:45:05 -0800

    change link tag

--- src/Components/navigationSidebar/NavigationSideBar.js
+++ src/Components/navigationSidebar/NavigationSideBar.js
@@ -1,65 +1,74 @@
 import { useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import profileImage from "../../assets/new_img1.jpg"
+import profileImage from "../../assets/new_img1.jpg";
 import "./navigation.css";
 function NavigationSideBar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const navRef = useRef();
-const toggleNavbar = () => {
+  const toggleNavbar = () => {
     setIsNavOpen(!isNavOpen);
   };
- return (
-   <>
-    <header className="header-container">
-      <div className="nav-btn" onClick={toggleNavbar}>
-        {isNavOpen ? <FaTimes /> : <FaBars />}
-      </div>
-      <div className="user">
-        <img src={profileImage} alt="mo. imrankhan"/>
-        <h3 className="name">Mohmad Imrankhan</h3>
-        <p className="post">front end developer</p>
-      </div>
-      <div className="share">
-            <Link
-              to="https://www.instagram.com/imrankhanpathan35/"
-              target="_blank"
-            >
-              <i className="fa-brands fa-square-instagram"></i>
-            </Link>
-            <Link
-              to="https://www.linkedin.com/in/imran-khan-pathan-881375265/"
-              target="_blank"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </Link>
-            <Link to="https://github.com/pathanimran4373" target="_blank">
-              <i className="fa fa-github" aria-hidden="true"></i>
-            </Link>
-            <Link to="">
-              <i className="fa-brands fa-square-whatsapp"></i>
-            </Link>
-          </div>
-      <nav className={`navbar ${isNavOpen ? "responsive_nav" : ""}`} ref={navRef}>
-        <Link className="menu" to="/" onClick={toggleNavbar}>
-          HOME
-        </Link>
-        <Link className="menu" to="/about" onClick={toggleNavbar}>
-          ABOUT
-        </Link>
-        <Link className="menu" to="/education" onClick={toggleNavbar}>
-          EDUCATION
-        </Link>
-        <Link className="menu" to="/contact" onClick={toggleNavbar}>
-          CONTACT
-        </Link>
-        <Link className="menu" to="/project" onClick={toggleNavbar}>
-          PROJECT
-        </Link>
-      </nav>
-    </header>
-   </>
+  return (
+    <>
+      <header className="header-container">
+        <div className="nav-btn" onClick={toggleNavbar}>
+          {isNavOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <div className="user">
+          <img src={profileImage} alt="mo. imrankhan" />
+          <h3 className="name">Mohmad Imrankhan</h3>
+          <p className="post">front end developer</p>
+        </div>
+        <div className="share">
+          <a
+            href="https://www.instagram.com/imrankhanpathan35/"
+            target="_blank"
+            title="instagram"
+          >
+            <i className="fa-brands fa-square-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/imran-khan-pathan-881375265/"
+            target="_blank"
+            title="linkedin"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/pathanimran4373"
+            target="_blank"
+            title="github"
+          >
+            <i className="fa fa-github" aria-hidden="true"></i>
+          </a>
+          <a href="#" title="whatsapp">
+            <i className="fa-brands fa-square-whatsapp"></i>
+          </a>
+        </div>
+
+        <nav
+          className={`navbar ${isNavOpen ? "responsive_nav" : ""}`}
+          ref={navRef}
+        >
+          <Link className="menu" to="/" onClick={toggleNavbar}>
+            HOME
+          </Link>
+          <Link className="menu" to="/about" onClick={toggleNavbar}>
+            ABOUT
+          </Link>
+          <Link className="menu" to="/education" onClick={toggleNavbar}>
+            EDUCATION
+          </Link>
+          <Link className="menu" to="/contact" onClick={toggleNavbar}>
+            CONTACT
+          </Link>
+          <Link className="menu" to="/project" onClick={toggleNavbar}>
+            PROJECT
+          </Link>
+        </nav>
+      </header>
+    </>
   );
 }
 export default NavigationSideBar;
-
