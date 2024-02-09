3e0a5b093a9e794955bbd884fe48af1c1767d8b0
Your Name <pathanimran4373@gmail.com>
2024-02-08 23:18:27 -0800

    update app component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React, { Suspense, lazy } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./Components/ErrorBoundries";
 import Navbar from "./Components/navigationSidebar/NavigationSideBar";
-import Home from "./pages/home-page/HomePage";
+import PageNotFound from "./pages/pageNotFound/PajeNotfound";
+import LoadingPage from "./Components/loader/LoadingPage";
+
+// Lazily loaded components
+const Home = lazy(() => import("./pages/home-page/HomePage"));
 const About = lazy(() => import("./pages/about-page/AboutPage"));
 const Education = lazy(() => import("./pages/education-page/EducationPage"));
 const Contact = lazy(() => import("./pages/contact-page/ContactPage"));
 const Project = lazy(() => import("./pages/project-page/ProjectPage"));
-import PageNotFound from "./pages/pageNotFound/PajeNotfound"
-import LoadingPage from "./Components/loader/LoadingPage";
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <ErrorBoundary>
-          <Navbar />
-        </ErrorBoundary>
-        <Suspense fallback={<LoadingPage/>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/education" element={<Education />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="*" element={< PageNotFound />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Navbar />
+      </ErrorBoundary>
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          {/* Define routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
