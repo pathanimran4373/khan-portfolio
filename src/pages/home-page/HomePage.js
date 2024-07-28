bf6086abbc29c75fc1023685303de764b50e3bad
mohmadimran <pathanimran4373@gmail.com>
2024-07-28 13:34:12 +0530

    change the text of home page

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./homePage.css";
-import resumePdf from "../../assets/cv.pdf"
+import resumePdf from "../../assets/cv.pdf";
 
 function HomePage() {
   return (
@@ -16,12 +16,22 @@ function HomePage() {
             I am a front end developer, I love my job and do well!
           </h2>
           <br />
-          <p>
+          {/* <p>
             A result oriented professional with 2+ years of experience Front
             End/UI Developer with “Bit Hub Technology Pvt. ltd. Pune(MH)” from December
             2021 to April 2024 & extensive experience in all the stages of
             Designing, Developing, Debugging. <br />
             My projects are...
+          </p> */}
+          <p>
+            Hi, I'm Mo.Imrankhan, a passionate Front End Developer. I love for
+            creating dynamic and engaging web experiences. Over the years, I
+            have honed my skills in HTML, CSS, JavaScript, and modern frameworks
+            like React. I strive to build websites that not only look great on
+            any device but also provide a seamless user experience. My expertise
+            extends to performance optimization, accessibility, and the latest
+            web technologies, enabling me to deliver fast, accessible, and
+            interactive web applications.
           </p>
         </div>
         <div className="home_btn">
